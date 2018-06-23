64895aeae0f5262dbc8361d67d83b92dbdbec715
Ryker Rumsey <rykerrumsey@outlook.com>
2018-06-22 23:44:57 -0600

    refactored app to use electron and webpack

---
+++ src/renderer/index.js
@@ -0,0 +1,13 @@
+import './styles.scss'
+
+import * as monaco from 'monaco-editor';
+
+function menu() {
+  let menu = document.createElement("DIV")
+  menu.setAttribute("id", "menu")
+
+  return menu;
+}
+// monaco.editor.create(document.getElementById('app'), {
+// 	language: 'html'
+// });
